d814d208306fa38357f275c40ecd1cc3ae7d5ad4
Lialah Barrer <lrbarrer@gmail.com>
2019-09-13 21:07:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,7 @@ end
   
 def assign_rooms(speakers)
   speaker.map.with_index { |speaker, room| 
-    "Hello, #{speaker}! You'll be assigned to room #{room+1}" }
+    "Hello, #{speaker}! You'll be assigned to room #{room+1}" 
+  }
   
 end
