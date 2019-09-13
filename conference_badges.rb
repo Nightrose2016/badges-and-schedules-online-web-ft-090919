fd03dadac6eb25dc16e586861ae9f9d06eaf0299
Lialah Barrer <lrbarrer@gmail.com>
2019-09-13 20:54:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."  
 end
 
+def batch_badge_creator
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
 def assign_rooms
   
 end
