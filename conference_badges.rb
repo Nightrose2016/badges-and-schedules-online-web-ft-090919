1b580125bf34955f5700604552c2f48d95c276a7
Lialah Barrer <lrbarrer@gmail.com>
2019-09-13 21:06:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,9 @@ def batch_badge_creator(names)
   names.map { |name| badge_maker(name)}
 end
   
-def assign_rooms (speakers)
-  speaker.map.with_index { |speaker, room| "Hello, #{speaker}! You'll be assigned to room #{room+1}"}
+def assign_rooms(speakers)
+  speaker.map.with_index { |speaker, room| "Hello, #{speaker}! You'll be assigned to room #{room+1}"
+    
+  }
   
 end
